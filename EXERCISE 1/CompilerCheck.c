2ff6c17c04581aa576decbf2699348d6dffcc54b
behrad <behmardibehrad@gmail.com>
2017-05-23 21:32:43 -0400

    added folder

--- EXERCISE 1/CompilerCheck.c
+++ EXERCISE 1/CompilerCheck.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+// include is a way to import the contents of one file into the program's source file.
+// the .h extension stands for header files which contains list of function to use here.
 
 /* This is a comment. */
 
